c6e75659f7c78f6beb3b46a507998be44275d727
Aisyah Aulia Angelinee <vincentius.clarishna@gmail.com>
2023-10-26 12:53:27 +0700

    HOMEPAGE-INPROGRESS

--- controllers/mainPageController.js
+++ controllers/mainPageController.js
@@ -1,9 +1,10 @@
-const { Item } = require('../models')
+const { Item, Category } = require('../models')
 
 class MainPage {
     static async showHomePage(req, res) {
         try {
             const dataItem = await Item.findAll()
+            console.log(dataItem.Category, "<<<<<<<<<<<<");
             res.render('home-page.ejs', { dataItem })
         } catch (error) {
             console.log(error);
