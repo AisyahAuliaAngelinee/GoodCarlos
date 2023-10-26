18d527a619153758e8829a494bda1e3bd869a96a
fionadrsnt <project.fionadarasanti@gmail.com>
2023-10-27 05:05:49 +0700

    change data category and ejs picture

--- controllers/mainPageController.js
+++ controllers/mainPageController.js
@@ -2,22 +2,24 @@ const { Item, Category } = require('../models')
 const formattedCurr = require('../helpers/formattedCurrency')
 
 class MainPage {
-    static async showHomePage(req, res) {
-        // console.log(Item);
-        try {  
-            const test = await Category.findOne({
-                where: {
-                    name: 'Foods'
-                },
-                include: Item
-            })
-            // console.log(test, 'hahaha');
-            res.render('home-page.ejs', {data: test, formattedCurr })
-        } catch (error) {
-            console.log(error);
-            res.send(error)
+        static async showHomePage(req, res) {
+            try {  
+                // const test = await Category.findOne({
+                //     where: {
+                //         name: 'Foods'
+                //     },
+                //     include: Item
+                // })
+                const data = await Item.findAll({
+                    include: Category
+                })
+                res.render('home-page.ejs', { formattedCurr, data})
+            } catch (error) {
+                console.log(error);
+                res.send(error)
+            }
         }
-    }
+    
     static async upVote(req, res) {
         try {
             await Item.upVote(+req.params.id)
